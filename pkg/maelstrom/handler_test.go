2b2adb4d584754185a4ca7f8703592b0a437a6a2
James Cooper <james@bitmechanic.com>
2019-09-18 12:46:16 -0700

    Uncomment more tests

--- pkg/maelstrom/handler_test.go
+++ pkg/maelstrom/handler_test.go
@@ -28,50 +28,50 @@ func TestHealthCheckStopsContainerOnFailure(t *testing.T) {
 	})
 }
 
-//func TestHealthCheckKeepsContainerOnSuccess(t *testing.T) {
-//	wrapTest(t, func() {
-//		GivenExistingContainer(t).
-//			WhenHTTPRequestReceived().
-//			WhenHealthCheckTimeoutElapses().
-//			ThenContainerIsStarted()
-//	})
-//}
-//
-//func TestStopDrainsRequestsBeforeStoppingContainers(t *testing.T) {
-//	wrapTest(t, func() {
-//		GivenExistingContainer(t).
-//			WhenContainerIsHealthy().
-//			WhenNLongRunningRequestsMade(5).
-//			WhenStopRequestReceived().
-//			ThenContainerIsStopped().
-//			ThenSuccessfulRequestCountEquals(5)
-//	})
-//}
-//
-//func TestRestartsContainerIfRequestArrivesAfterStopping(t *testing.T) {
-//	wrapTest(t, func() {
-//		GivenExistingContainer(t).
-//			WhenStopRequestReceived().
-//			ThenContainerIsStopped().
-//			WhenHTTPRequestReceived().
-//			ThenContainerIsStarted()
-//	})
-//}
-//
-//func TestStopsContainerIfIdle(t *testing.T) {
-//	wrapTest(t, func() {
-//		GivenExistingContainerWithIdleTimeout(t, 1).
-//			WhenIdleTimeoutElapses().
-//			WhenAutoscaleRuns().
-//			ThenContainerIsStopped()
-//	})
-//}
-//
-//func TestRestartsContainerWhenComponentUpdated(t *testing.T) {
-//	wrapTest(t, func() {
-//		GivenExistingContainer(t).
-//			WhenComponentIsUpdated().
-//			WhenHTTPRequestReceived().
-//			ThenContainerIsStartedWithNewVersion()
-//	})
-//}
+func TestHealthCheckKeepsContainerOnSuccess(t *testing.T) {
+	wrapTest(t, func() {
+		GivenExistingContainer(t).
+			WhenHTTPRequestReceived().
+			WhenHealthCheckTimeoutElapses().
+			ThenContainerIsStarted()
+	})
+}
+
+func TestStopDrainsRequestsBeforeStoppingContainers(t *testing.T) {
+	wrapTest(t, func() {
+		GivenExistingContainer(t).
+			WhenContainerIsHealthy().
+			WhenNLongRunningRequestsMade(5).
+			WhenStopRequestReceived().
+			ThenContainerIsStopped().
+			ThenSuccessfulRequestCountEquals(5)
+	})
+}
+
+func TestRestartsContainerIfRequestArrivesAfterStopping(t *testing.T) {
+	wrapTest(t, func() {
+		GivenExistingContainer(t).
+			WhenStopRequestReceived().
+			ThenContainerIsStopped().
+			WhenHTTPRequestReceived().
+			ThenContainerIsStarted()
+	})
+}
+
+func TestStopsContainerIfIdle(t *testing.T) {
+	wrapTest(t, func() {
+		GivenExistingContainerWithIdleTimeout(t, 1).
+			WhenIdleTimeoutElapses().
+			WhenAutoscaleRuns().
+			ThenContainerIsStopped()
+	})
+}
+
+func TestRestartsContainerWhenComponentUpdated(t *testing.T) {
+	wrapTest(t, func() {
+		GivenExistingContainer(t).
+			WhenComponentIsUpdated().
+			WhenHTTPRequestReceived().
+			ThenContainerIsStartedWithNewVersion()
+	})
+}
